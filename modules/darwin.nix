c5e0f2a246a5d64ead80f482371bb89942248b74
Brancen Gregory <brancengregory@gmail.com>
2025-06-29 12:43:52 -0500

    Init darwin config

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -3,16 +3,83 @@
 {
 	nix.enable = true;
 
-	# Set the primary user for user-specific system settings
-	system.primaryUser = "brancengregory";
+	system = {
+		# Set the primary user for user-specific system settings
+		primaryUser = "brancengregory";
 
-	# Set system defaults
-	system.defaults.screencapture.location = "~/Downloads";
+		defaults = {
+			NSGlobalDomain = {
+				AppleInterfaceStyle = Dark;
+				ApplePressAndHoldEnabled = true;
+				AppleScrollerPagingBehavior = true;
+				AppleShowAllExtensions = true;
+				AppleShowAllFiles = false;
+				AppleShowScrollBars = "WhenScrolling";
+				# InitialKeyRepeat = ; # Add value
+				# KeyRepeat = ; # Add value
+				NSAutomaticCapitalizationEnabled = false;
+				NSAutomaticDashSubstitutionEnabled = false;
+				NSAutomaticInlinePredictionEnabled = false;
+				NSAutomaticPeriodSubstitutionEnabled = false;
+				NSAutomaticQuoteSubstitutionEnabled = false;
+				NSAutomaticSpellingCorrectionEnabled = false;
+				NSDisableAutomaticTermination = true;
+				NSDocumentSaveNewDocumentsToCloud = false;
+				_HIHideMenuBar = true;
+				"com.apple.sound.beep.feedback" = 0;
+				"com.apple.sound.beep.volume" = 0;
+				"com.apple.swipescrolldirection" = false;
+				# "com.apple.trackpad.scaling" = ; # Add value
+			};
+			controlcenter = {
+				AirDrop = 24;
+				BatteryShowPercentage = true;
+				Bluetooth = true;
+				Display = true;
+				FocusModes = true;
+				NowPlaying = true;
+				Sound = true;
+			};
+			dock = {
+				appswitcher-all-displays = true;
+				autohide = true;
+				persistent-others = [
+					"~/Downloads"
+				];
+				show-recents = false;
+				wvous-bl-corner = 1;
+				wvous-br-corner = 1;
+				wvous-tl-corner = 1;
+				wvous-tr-corner = 1;
+			};
+			finder = {
+				AppleShowAllExtensions = true;
+				AppleShowAllFiles = false;
+				FXEnableExtensionChangeWarning = false;
+				FXPreferredViewStyle = "Nlsv";
+				NewWindowTarget = "Home";
+				ShowHardDrivesOnDesktop = true;
+				ShowPathbar = true;
+				ShowStatusBar = true;
+				_FXShowPosixPathInTitle = true;
+			};
+			loginwindow = {
+				GuestEnabled = false;
+			};
+			menuExtraClock = {
+				ShowAMPM = true;
+				ShowDate = 1;
+			};
+			screencapture = {
+				location = "~/Downloads";
+			};
+		};
+	};
 
 	# Ensure nix-daemon is running
 	nix.package = pkgs.nix;
 
-	# Definte the user account for nix-darwin
+	# Define the user account for nix-darwin
 	users.users.brancengregory = {
 		home = "/Users/brancengregory";
 	};
