7997b0996c17be1b747e9914bb8e8cfceaa04bcf
Brancen Gregory <brancengregory@gmail.com>
2026-01-12 19:57:08 -0600

    fix(powerhouse): enable home-manager user packages and global pkgs
    Enables useGlobalPkgs and useUserPackages to resolve hm-session-vars.sh sourcing issues and ensure packages like ghostty are correctly installed in the VM environment. Also includes pending VM variant optimizations.

--- hosts/powerhouse/config.nix
+++ hosts/powerhouse/config.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
   imports = [
     ../../modules/os/common.nix # Universal settings
     ../../modules/os/nixos.nix # Common NixOS settings
@@ -12,6 +12,22 @@
   networking.hostName = "powerhouse";
   nixpkgs.config.allowUnfree = true;
 
+  # Optimize VM Performance
+  virtualisation.vmVariant = {
+    virtualisation = {
+      memorySize = 8192; # 8GB RAM
+      cores = 4;
+      graphics = true;
+    };
+    # Override Nvidia driver in VM to use standard QEMU graphics
+    services.xserver.videoDrivers = pkgs.lib.mkForce [ "modesetting" ];
+    hardware.nvidia.modesetting.enable = pkgs.lib.mkForce false;
+    hardware.nvidia.open = pkgs.lib.mkForce false;
+  };
+
+  home-manager.extraSpecialArgs = { inherit inputs; };
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
   home-manager.users.brancengregory = {
     imports = [
       ../../users/brancengregory/home.nix
