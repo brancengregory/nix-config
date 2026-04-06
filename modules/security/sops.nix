6040f2edcf4d59370f0e2957fd92dc7a6b351d62
Brancen Gregory <brancengregory@gmail.com>
2026-04-05 22:04:59 -0500

    fix: remove text keyfile from sops config

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -10,9 +10,6 @@
   # This corresponds to the host_powerhouse key in .sops.yaml
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
-  # The path to the user's age key (optional for system, mainly for home-manager but good to have)
-  sops.age.keyFile = "/home/brancengregory/.config/sops/age/keys.txt";
-
   # Restic Backup Secrets
   sops.secrets."restic/password" = {};
   sops.secrets."restic/env" = {};
